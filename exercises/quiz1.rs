33baa9aafe70ccafde946e25ca584f963d62e6bf
SomyaRanjanSahu <somyasahu10@gmail.com>
2024-06-12 17:18:45 +0530

    Quiz1 & Primitive Types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32{
+    if apples <= 40 {
+        apples * 2
+    } else {
+        apples
+    }
+
+}
 
 // Don't modify this function!
 #[test]
